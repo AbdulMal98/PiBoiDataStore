c148a620768f5ca531f94f6ea704612324699069
Jacob Baker <waffletoast2@yahoo.com>
2021-06-30 19:13:10 -0500

    Moved bme280.py to sensor_handler package; removed unnecessary class; setup main_driver.py; cleaned up variable names in hypsometric formula; added .gitignore;

--- client/httpclient.py
+++ client/httpclient.py
@@ -1,7 +1,16 @@
+import base64
+
 import requests
+
+user: str = "Mal"
+password: str = "MGSgQYPRuI"
 
 
 def postAtp(temperature, pressure, altitude, timestamp):
     datadict = {"timestamp": timestamp, "altitude": altitude, "temperature": temperature, "pressure": pressure}
-    r = requests.post("http://localhost:8080", json=datadict)
+    r = requests.post("http://localhost:8080", json=datadict, headers=getBasicAuthHeader())
     print(r.status_code)
+
+
+def getBasicAuthHeader():
+    return {"Authorization": base64.b64encode(user + ":" + password)}
